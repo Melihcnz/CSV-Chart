6b85ee72f6486535f71deb56db8fb8d6c989d0a3
Melih Canaz <92990734+Melihcnz@users.noreply.github.com>
2024-12-12 19:05:34 +0300

    style değişimi

--- src/components/Charts/components/BarChart.js
+++ src/components/Charts/components/BarChart.js
@@ -1,33 +1,47 @@
-import React from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
-import InfoBox from './InfoBox';
+import React from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+import InfoBox from "./InfoBox";
 
 const CustomBarChart = ({ data, header, averages }) => (
-    <div className="chart-box">
-        <h3>{header} Dağılımı</h3>
-        <BarChart
-            width={500}
-            height={300}
-            data={data}
-            margin={{ top: 5, right: 20, left: 15, bottom: 20 }}
-        >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis 
-                dataKey="name"
-                label={{ value: 'Marka', position: 'insideBottom', offset: -15 }}
-            />
-            <YAxis 
-                label={{ value: header, angle: -90, position: 'insideLeft', offset: 10 }}
-            />
-            <Tooltip />
-            <Legend verticalAlign="top" height={36}/>
-            <Bar dataKey={header} fill="#82ca9d" />
-        </BarChart>
-        <InfoBox 
-            label={`Ortalama ${header}`} 
-            value={averages[header]} 
-        />
-    </div>
+  <div className="chart-box">
+    <h3>{header} Dağılımı</h3>
+    <BarChart
+      width={500}
+      height={300}
+      data={data}
+      margin={{ top: 5, right: 20, left: 15, bottom: 20 }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis
+        dataKey="name"
+        label={{ value: "Marka", position: "insideBottom", offset: -15 }}
+      />
+      <YAxis
+        label={{
+          value: header,
+          angle: -90,
+          position: "insideLeft",
+          offset: 10,
+        }}
+      />
+      <Tooltip
+        cursor={{ strokeDasharray: "3 3" }}
+        wrapperStyle={{ zIndex: 1000 }}
+        position={{ x: "auto", y: "auto" }}
+      />
+      <Legend verticalAlign="top" height={36} />
+      <Bar dataKey={header} fill="#82ca9d" />
+    </BarChart>
+    <InfoBox label={`Ortalama ${header}`} value={averages[header]} />
+  </div>
 );
 
 export default CustomBarChart;
